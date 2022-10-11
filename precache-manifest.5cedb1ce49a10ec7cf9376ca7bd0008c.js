939cf098f010c268517e51cade8dadfd32150a26
Aleksandrttt34 <aleksandrttt.34.34@gmail.com>
2022-10-11 23:08:33 +0300

    Updates

---
+++ precache-manifest.5cedb1ce49a10ec7cf9376ca7bd0008c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d629d483f0a0b180b420",
+    "url": "/react-shop/static/css/main.2e08b77f.chunk.css"
+  },
+  {
+    "revision": "d629d483f0a0b180b420",
+    "url": "/react-shop/static/js/main.71a41afd.chunk.js"
+  },
+  {
+    "revision": "75bd495123a48ea82bb9",
+    "url": "/react-shop/static/js/runtime~main.0bd8b961.js"
+  },
+  {
+    "revision": "89a34c04818a758186dd",
+    "url": "/react-shop/static/js/2.5667b7f5.chunk.js"
+  },
+  {
+    "revision": "3ebb7536bbb32cdce6362d3544354861",
+    "url": "/react-shop/index.html"
+  }
+];
